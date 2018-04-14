0835f0696d5bd97468c8dc04b9d7a2d7228285de
Arpit Shah <collegeimprovements@gmail.com>
2018-04-14 14:24:48 +0530

    Updates

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Grishneshwar',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
---
+++ path----d1f958e6604a3e04cd58.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Grishneshwar"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----d1f958e6604a3e04cd58.js.map
